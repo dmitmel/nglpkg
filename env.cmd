abe591eba49aedb5a2754571cbb28386860f88fa
20kdc <asdd2808@gmail.com>
2019-06-29 19:49:31 +0100

    Actually fix Windows support for the SDK

--- env.cmd
+++ env.cmd
@@ -1,5 +1,5 @@
 rem Assists in environment setup
-set LUA_PATH="%LUA_PATH%;%~dp0\?.lua"
-set LUA_CPATH="%LUA_CPATH%;%~dp0\reference\NT %PROCESSOR_ARCHITECTURE%\?.dll"
+set LUA_PATH=%LUA_PATH%;%~dp0\?.lua
+set LUA_CPATH=%LUA_CPATH%;%~dp0\reference\NT %PROCESSOR_ARCHITECTURE%\?.dll
 "%~dp0\reference\NT %PROCESSOR_ARCHITECTURE%\luajit" %*
 
