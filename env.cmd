f7dfc715c6243bb88b3ce801483e4f2bf89e7b6e
20kdc <asdd2808@gmail.com>
2019-06-29 19:35:07 +0100

    Fix Windows env.cmd

--- env.cmd
+++ env.cmd
@@ -1,5 +1,5 @@
-rem NGLPKG bootstrap script - this & PROGRAM are CC0
+rem Assists in environment setup
 set LUA_PATH="%LUA_PATH%;%~dp0\?.lua"
 set LUA_CPATH="%LUA_CPATH%;%~dp0\reference\NT %PROCESSOR_ARCHITECTURE%\?.dll"
-"%~dp0\NT %PROCESSOR_ARCHITECTURE%\luajit" "%~dp0\binary" %*
+"%~dp0\reference\NT %PROCESSOR_ARCHITECTURE%\luajit" %*
 
